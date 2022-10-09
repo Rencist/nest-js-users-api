54b5af42613fd46423c23e2d60683eeb8f053a43
Rencist <dafianakhdan@gmail.com>
2022-10-09 21:45:43 +0700

    feat: post create user

---
+++ src/typeorm/entities/User.ts
@@ -0,0 +1,15 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column({ unique: true })
+  username: string;
+  @Column()
+  password: string;
+  @Column()
+  createdAt: Date;
+  @Column({ nullable: true })
+  socialMedia: string;
+}
---
+++ src/users/controllers/users/users.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { Body } from '@nestjs/common/decorators/http/route-params.decorator';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+import { UsersService } from 'src/users/services/users/users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Get()
+  getUsers() {}
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    this.userService.createUser();
+  }
+}
